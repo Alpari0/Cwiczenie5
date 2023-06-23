8e5bd0134849e4b8621fcde4c8427e6649c29cbb
Alpari0 <adnesspl@gmail.com>
2023-06-24 00:01:49 +0200

    Push from local GIT

---
+++ 1.N_liczb_pierwszych.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int zliczanie = 0;
+        int liczba = 2;
+
+        while (zliczanie < n)
+        {
+            if (sprawdzCzyPierwsza(liczba))
+            {
+                Console.WriteLine(liczba);
+                zliczanie++;
+            }
+
+            liczba++;
+        }
+    }
+
+    static bool sprawdzCzyPierwsza(int liczba)
+    {
+        for (int i = 2; i <= Math.Sqrt(liczba); i++)
+        {
+            if (liczba % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
